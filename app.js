db05dc50a2b25c57fcab59aa8349ec5fd65559f7
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2021-09-17 22:33:23 +0300

    default slide

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll('.slide');
+"use strict";
+function slidesPlugin (activeSlide) {
+    const slides = document.querySelectorAll('.slide');
 
-slides.forEach(slide => {
-    slide.addEventListener('click', () => {
-        clearActiveClasses();
-        slide.classList.add('active');
+    slides[activeSlide].classList.add('active');
+    
+    slides.forEach(slide => {
+        slide.addEventListener('click', () => {
+            clearActiveClasses();
+            slide.classList.add('active');
+        });
     });
-});
-
-function clearActiveClasses() {
-    slides.forEach(slide =>{
-        slide.classList.remove('active');
-    });
+    
+    function clearActiveClasses() {
+        slides.forEach(slide =>{
+            slide.classList.remove('active');
+        });
+    }
 }
+slidesPlugin(0);
